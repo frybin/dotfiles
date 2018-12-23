770887e5fe6cee9e513581f34fdaccdf45f5f2a6
frybin <fredrybin@gmail.com>
2018-12-23 13:11:45 -0500

    New Packages

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo mount -o remount,size=12G,noatime /tmp
 
-trizen -Su zsh xorg i3-gaps pcsc-tools yubico-yubioath-desktop-git nerd-fonts-complete terminator rofi compton python-pywal feh conky ranger ccid opensc lsof scrot i3lock-fancy-git arandr ntp light-git bluez bluez-utils w3m udiskie htop network-manager-applet foxitreader wireshark-qt keychain gnupg stow cups system-config-printer neofetch vim pacman-contrib neofetch powertop zip unzip unrar p7zip speedtest-cli firefox linux linux-headers networkmanager openssh cronie xdg-user-dirs dosfstools ntfs-3g btrfs-progs exfat-utils autofs fuse3 fuseiso alsa-utils alsa-plugins pulseaudio pulseaudio-alsa pulseaudio-bluetooth cups-pdf qbittorrent thunderbird vlc code networkmanager-openvpn lastpass-cli polybar jq twmn-git notification-daemon acpi
+trizen -Su zsh xorg i3-gaps pcsc-tools yubico-yubioath-desktop-git nerd-fonts-complete terminator rofi compton python-pywal feh conky ranger ccid opensc lsof scrot i3lock-fancy-git arandr ntp light-git bluez bluez-utils w3m udiskie htop network-manager-applet foxitreader wireshark-qt keychain gnupg stow cups system-config-printer neofetch vim pacman-contrib neofetch powertop zip unzip unrar p7zip speedtest-cli firefox linux linux-headers networkmanager openssh cronie xdg-user-dirs dosfstools ntfs-3g btrfs-progs exfat-utils autofs fuse3 fuseiso alsa-utils alsa-plugins pulseaudio pulseaudio-alsa pulseaudio-bluetooth cups-pdf qbittorrent thunderbird vlc code networkmanager-openvpn lastpass-cli polybar jq twmn-git notification-daemon acpi firewalld 
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
