62f206a4126c962fa128173a30c3376f3738e2b1
frybin <fredrybin@gmail.com>
2019-01-16 20:33:53 -0500

    Added Multiple Monitors

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -7,5 +7,16 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
-polybar top &
-polybar bottom &
+# polybar top &
+# polybar bottom &
+
+
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar -c $HOME/.config/polybar/config top &
+		MONITOR=$m polybar -c $HOME/.config/polybar/config bottom &
+	done
+else
+	polybar -c $HOME/.config/polybar/config top &
+        polybar -c $HOME/.config/polybar/config bottom &
+fi
