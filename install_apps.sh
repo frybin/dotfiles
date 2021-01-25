2000cfa0b6a1e49da1c70436f9d6787db1c26cb4
Fred Rybin <fredrybin@gmail.com>
2021-01-25 10:22:16 -0500

    Beep Boop

--- install_apps.sh
+++ install_apps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yay -Sy --noconfirm firefox qpdfview qtfm libappindicator-gtk3 xournalpp pavucontrol thunderbird thunderbird-extension-enigmail birdtray
-yay -Sy --noconfirm hamsket-bin slack-desktop discord zoom
+yay -Sy --noconfirm firefox qpdfview qtfm libappindicator-gtk3 xournalpp pavucontrol thunderbird thunderbird-extension-enigmail
+yay -Sy --noconfirm lack-desktop discord zoom
 yay -Sy --noconfirm spotify visual-studio-code-bin system-config-printer 
 yay -Sy --noconfirm remmina freerdp libvncserver
